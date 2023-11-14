8e21483449d309f32da04761add91c9817eb59fa
pm.jaycci <pm.jaycci@gmail.com>
2023-11-14 17:59:02 +0900

    노티서버 셋팅

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # NotificationServer
+#golang #gRPC
+GOOS=linux GOARCH=amd64 go build -o game_server
---
+++ server/server_manager.go
@@ -0,0 +1,50 @@
+package server
+
+import (
+	"encoding/json"
+	global_grpc "project_yd/grpc"
+	packet "project_yd/server/server_packet"
+	"project_yd/util"
+	"time"
+)
+
+const SERVER_PORT = ":8082"
+
+func SendBroadcastMessageToClient(client global_grpc.GlobalGRpcService_GlobalGrpcStreamBroadcastServer, opcode int32, message string) error {
+	response := &global_grpc.GlobalGrpcBroadcast{
+		Opcode:  opcode,
+		Message: message,
+	}
+	if err := client.SendMsg(response); err != nil {
+		return err
+	}
+	return nil
+}
+
+func BroadcastBaseMessage(client global_grpc.GlobalGRpcService_GlobalGrpcStreamBroadcastServer, opcode int32, broadcastPacket interface{}) {
+	message, _ := json.Marshal(broadcastPacket)
+	SendBroadcastMessageToClient(client, opcode, string(message))
+}
+
+func SendDuplicateLogin(UUID string) {
+	for _, client := range BroadcastClients {
+		broadcastPacket := packet.BroadcastDuplicateLogin{}
+		broadcastPacket.UUID = UUID
+		BroadcastBaseMessage(client, util.DUPLICATE_LOGIN, broadcastPacket)
+	}
+}
+
+func SendHeartBeat() {
+	for {
+		println("Broadcast -- SendHeartBeat")
+		if len(BroadcastClients) > 0 {
+			for _, client := range BroadcastClients {
+				broadcastPacket := packet.BroadcastPacket{}
+				broadcastPacket.Message = "Send Heart Beat"
+				BroadcastBaseMessage(client, util.HEARTBEAT, broadcastPacket)
+			}
+		}
+		// 60초 대기
+		time.Sleep(60 * time.Second)
+	}
+}
